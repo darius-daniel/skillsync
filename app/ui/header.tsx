c541d1619d3f672dbed1d2e97defe640f164b4f3
Darius Daniel <dariusdaniel771@email.com>
2025-03-04 04:50:50 +0100

    Created a header component and added it to the root layout

---
+++ app/ui/header.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1">
+        <Link className="btn btn-ghost text-xl" href="/">SkillSync</Link>
+      </div>
+      <div className="flex gap-2">
+        <input type="text" placeholder="Search" className="input input-bordered focus:border-primary w-24 md:w-auto" />
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <Image
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                width={40}
+                height={40}
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li><a>Settings</a></li>
+            <li><a>Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
